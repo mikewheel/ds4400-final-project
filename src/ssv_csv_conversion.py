607be8ffa9e810395ede0e8afdd36a5754b976a1
Michael Wheeler <wheeler.m@husky.neu.edu>
2020-03-21 14:54:52 -0400

    Removed semicolon-delimited data files, requirements, code cleanup, etc

--- src/ssv_csv_conversion.py
+++ src/ssv_csv_conversion.py
@@ -3,31 +3,10 @@ import pandas as pd
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression, LinearRegression
 
-def main():
-    red_wines = pd.read_csv("wine_quality_red.csv")
-    white_wines = pd.read_csv("wine_quality_white.csv")
-    phi = basis_expansion(red_wines.head(5))
 
-    parser = ArgumentParser()
-    parser.add_argument("--model", nargs = 1, type=str, metavar="model")
-    args = parser.parse_args()
-    model = args.model[0]
-    if model == "logistic":
-        model = LogisticRegression("l2", random_state=0)
-    elif model == "linear":
-        model = LinearRegression(random_state = 0)
-    elif model == "svm":
-        model = SVC(kernel="linear", random_state=0)
-    else:
-        raise ValueError("Model type \"" + model + "\"not recognized")
-
-    red_X = red_wines.ix[:, range(11)]
-    print(red_wiens)
-    print(red_X)
-    print(phi)
-
-def basis_expansion(data, powers = None):
-    """Returns a basis expansion of input features.
+def basis_expansion(data: pd.DataFrame, powers=None):
+    """
+    Performs a basis expansion of arbitrary data features.
 
     Each feature (column) in data should have a corresponding element
     in powers. If the corresponding value for a feature <= 0
@@ -35,9 +14,9 @@ def basis_expansion(data, powers = None):
     corresponding value for a feature is positive, then every power between
     1 and the that value for the feature will be included in the basis
     expansion. If powers is None (as default), then each feature will not be
-     transformed or represent differently in the basis expansion. The last
-     element of the basis expansion will always be 1, to account for the
-     possibility of a nonzero intercept.
+    transformed or represent differently in the basis expansion. The last
+    element of the basis expansion will always be 1, to account for the
+    possibility of a nonzero intercept.
 
     Parameters
     ----------
@@ -62,21 +41,46 @@ def basis_expansion(data, powers = None):
     ValueError
         If powers is not the same length as the number of columns in data
     """
-    if (powers == None):
+    if not powers:
         powers = [1 for i in range(data.shape[1])]
-    elif (len(powers) != data.shape[1]):
+    
+    if len(powers) != data.shape[1]:
         raise ValueError("Length of maximal powers is not equal to number of features.")
 
     phi = pd.DataFrame()
+    
     for index, row in data.iterrows():
         new_row = []
         for i in range(len(row)):
             new_row += [pow(row[i], power) for power in range(1, powers[i]+1)]
         new_row.append(1)
-        phi = pd.concat([phi, pd.DataFrame([pd.Series(new_row)], index = [str(i)])],
+        phi = pd.concat([phi, pd.DataFrame([pd.Series(new_row)], index=[str(i)])],
                         ignore_index=True)
+    
     return phi
 
-if __name__ == "__main__":
-    main()
 
+if __name__ == "__main__":
+    
+    parser = ArgumentParser()
+    parser.add_argument("--model", nargs=1, type=str, metavar="model")
+    args = parser.parse_args()
+    model = args.model[0]
+    
+    red_wines = pd.read_csv("wine_quality_red.csv")
+    white_wines = pd.read_csv("wine_quality_white.csv")
+    phi = basis_expansion(red_wines.head(5))
+    
+    if model == "logistic":
+        model = LogisticRegression("l2", random_state=0)
+    elif model == "linear":
+        model = LinearRegression(random_state=0)
+    elif model == "svm":
+        model = SVC(kernel="linear", random_state=0)
+    else:
+        raise ValueError(f'Model type not recognized: "{model}"')
+    
+    red_X = red_wines.ix[:, range(11)]
+    print(red_wines)
+    print(red_X)
+    print(phi)
