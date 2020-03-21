89e95c8a515da459faeb14fc840c8453d0254860
Michael Wheeler <wheeler.m@husky.neu.edu>
2020-03-21 16:19:48 -0400

    Plots of joint feature distributions for red vs white wine data

---
+++ plotting/__main__.py
@@ -0,0 +1,48 @@
+"""
+Main module for exploratory visualization.
+
+Written by Michael Wheeler and Jay Sherman.
+"""
+from bokeh.io import export_png
+from bokeh.layouts import row
+from bokeh.plotting import figure
+from contextlib import suppress
+from numpy import histogram
+from pandas import DataFrame, read_csv
+from statistics import mean, stdev
+
+from config import DATA_DIR
+
+wine_data = {
+    "red": read_csv(DATA_DIR / "wine_quality_red.csv"),
+    "white": read_csv(DATA_DIR / "wine_quality_white.csv"),
+}
+
+wine_features = {
+    "fixed acidity",
+    "volatile acidity",
+    "citric acid",
+    "residual sugar",
+    "chlorides",
+    "free sulfur dioxide",
+    "total sulfur dioxide",
+    "density",
+    "pH",
+    "sulphates",
+    "alcohol",
+    "quality"
+}
+
+for feature in wine_features:
+    plots = {}
+    for color, df in wine_data.items():
+        # Shoutout to https://stackoverflow.com/a/45822244/8857601
+        binned_data, bin_edges = histogram(df[feature], 20)
+        plot_title = f'{feature} ({color}): μ={round(mean(df[feature]), 2)}, σ={round(stdev(df[feature]), 2)}, ' \
+                     f'n={len(df[feature])}'
+        plot = figure(title=plot_title)
+        plot.quad(top=binned_data, bottom=0, left=bin_edges[:-1], right=bin_edges[1:], line_color="white")
+        plots[color] = plot
+    big_plot = row(*[val for key, val in plots.items()])
+    with suppress(KeyboardInterrupt):
+        export_png(big_plot, filename=DATA_DIR / f'joint_distribution_{feature}.png')
