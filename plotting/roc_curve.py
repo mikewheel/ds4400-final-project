afddf1adf62868598151f2ffbbe9c556f7a15fe0
Michael Wheeler <wheeler.m@husky.neu.edu>
2020-04-24 15:14:19 -0400

    Stub file for ROC plotting

---
+++ plotting/roc_curve.py
@@ -0,0 +1,12 @@
+"""
+Code for generating ROC curves from a trained classifier.
+
+Written by Michael Wheeler and Jay Sherman.
+"""
+
+from config import make_logger
+
+logger = make_logger(__name__)
+
+if __name__ == "__main__":
+    pass
