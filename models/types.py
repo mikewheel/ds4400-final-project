15ec658e1063fd8abfdec73adb3018e600dfc999
Michael Wheeler <wheeler.m@husky.neu.edu>
2020-04-25 09:23:24 -0400

    Updated the list of abstract methods for factory classes.

--- models/types.py
+++ models/types.py
@@ -6,9 +6,31 @@ from abc import ABC, abstractmethod
 
 class ModelFactory(ABC):
     @abstractmethod
-    def ensure_output_dirs_exist(self) -> None:
+    def ensure_output_dirs_exist(self):
         pass
     
     @abstractmethod
     def generate_model_instances(self):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_coeffs(model):
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def get_error(model, X, y):
+        pass
+    
+    @abstractmethod
+    def best_model(self):
+        pass
+    
+    @abstractmethod
+    def target_output_dir(self):
+        pass
+    
+    @abstractmethod
+    def report_test_results(self, train_x, valid_x, test_x, train_y, valid_y, test_y):
         pass
